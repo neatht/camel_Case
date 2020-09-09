41697879e5c39f7731dd5369c2a55f1aba8499fb
Taylor Johnston <taylorjohnston@mail.com>
2020-09-09 22:28:50 +1000

    :rocket: Allow React and Express to deploy together

--- api/dist/index.js
+++ api/dist/index.js
@@ -3,12 +3,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+dotenv_1.default.config();
 const app = express_1.default();
-const port = 5000;
-// Route to default home page
-app.get("/", (req, res) => {
-    res.send("Hello");
+const port = process.env.SERVER_PORT || 5000;
+// Serve static react build
+app.use(express_1.default.static(path_1.default.join(__dirname, 'client/build')));
+// Test
+app.get('/api/getList', (req, res) => {
+    const list = ['item1', 'item2'];
+    res.json(list);
+});
+// Route to React app by default
+app.get("*", (req, res) => {
+    res.sendFile(path_1.default.join(__dirname + 'client/build/index.html'));
 });
 // Start the Express sever
 app.listen(port, () => {
--- api/src/index.ts
+++ api/src/index.ts
@@ -10,6 +10,12 @@ const port = process.env.SERVER_PORT || 5000;
 // Serve static react build
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+// Test
+app.get('/api/getList', (req, res) => {
+	const list = ['item1', 'item2'];
+	res.json(list);
+})
+
 // Route to React app by default
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname+'client/build/index.html'));
