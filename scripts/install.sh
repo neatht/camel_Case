b6e9d1824abfd8c407d26395f450721d74ee951b
Nathan Harvey <neathtthefirst@gmail.com>
2020-09-15 01:40:50 +1000

    ✨Add front end to install script, and both to run script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,3 +2,7 @@ echo preinstall
 pushd /home/ec2-user/application/api
 npm install
 popd
+pushd /home/ec2-user/application/client
+yarn add
+popd
+echo done
--- scripts/startServer.sh
+++ scripts/startServer.sh
@@ -1,1 +1,8 @@
 echo startServer
+pushd /home/ec2-user/application/api
+npm start
+popd
+pushd /home/ec2-user/application/client
+yarn start
+popd
+echo done
