0715ced7a43789e0df95c44265158d8a0af15350
Taylor Johnston <taylorjohnston@mail.com>
2020-09-18 13:39:05 +1000

    :white_check_mark: Add quick user details testing to front page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
+import { Spin } from 'antd';
 
 import Header from '../components/Header';
 
 function Home() {
+    const { user, isAuthenticated, isLoading } = useAuth0();
+
+    if (isLoading) {
+        return <Spin size="large"/>
+    }
+
     return(
         <div className="App">
             <Header pageKey="home" />
             <h1><span role="img" aria-hidden="true">🐪</span> camel_Case</h1>
+            {isAuthenticated
+            ? <div>
+                <h2> User: {user.name} </h2>
+                <p> Email: {user.email} </p>   
+              </div>
+            : <div>
+                <h2> Not logged in </h2>
+              </div>
+            }
         </div>
     );
 }
