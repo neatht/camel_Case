aff777ea0b3dda32eb700f8c306612fec2ed9e9c
Kevin Kim <kwkim@student.unimelb.edu.au>
2020-09-25 11:46:21 +0800

    remove user queries to db

--- api/src/queries/user.ts
+++
@@ -1,2 +0,0 @@
-export const getUserById = "SELECT * FROM public.user u WHERE u.user_id = $1";
-export const addUser = "INSERT INTO public.user VALUES ($1, $2, $3, $4)";
--- api/src/routes/db-routes.ts
+++ api/src/routes/db-routes.ts
@@ -1,29 +1,6 @@
 import express from "express";
 import client from "../config/postgres";
-import { getUserById, addUser } from "../queries/user";
 
 const router = express.Router();
-
-router.get('/getUser/:userId', (req, res) => {
-    client
-        .query(getUserById, [req.params.userId])
-        .then(result => {
-            res.json(result.rows);
-        })
-        .catch(err => {
-            throw err;
-        });
-});
-
-router.post('/addUser', (req, res) => {
-    client
-        .query(addUser, [req.body.row])
-        .then(result => {
-            res.json(result.rows[0]);
-        })
-        .catch(err => {
-            throw err;
-        });
-});
 
 export default router;
