c338ce546e04ea1af9ecc81816c73b934a897013
Nathan Harvey <neathtthefirst@gmail.com>
2020-09-28 17:19:37 +1000

    🐛 Add iptables reroute to preinstall

--- scripts/startServer.sh
+++ scripts/startServer.sh
@@ -1,5 +1,5 @@
 echo startServer
 pushd /home/ec2-user/application/api
-pm2 start ./dist/index.js --name "APIBackend" --log "/home/ecc2-user/application/log.txt" --time
+pm2 start ./dist/index.js --name "APIBackend" --log "/home/ec2-user/application/log.txt" --time
 popd
 echo done
