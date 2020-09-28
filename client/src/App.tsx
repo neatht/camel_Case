a83171bbe3a0fc43df6043ba6dc04a91e82317d2
Edward Hinrichsen <edward@pop-os.localdomain>
2020-09-28 17:58:12 +1000

    :recycle: Add CSS variables

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import 'antd/dist/antd.css';
+import './css/theme.css';
 import './css/base.css';
 import './css/layout.css';
-import './css/state.css';
-import 'antd/dist/antd.css';
+
 
 import Home from './pages/Home';
 import Profile from './pages/Profile';
