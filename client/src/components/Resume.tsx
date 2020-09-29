c20ab5d66779b54f12f61a8aae29be33f5f5df99
Edward Hinrichsen <edward@pop-os.localdomain>
2020-09-29 16:17:19 +1000

    :sparkles: Add ResumeEntry Component

--- client/src/components/Resume.tsx
+++ client/src/components/Resume.tsx
@@ -6,6 +6,7 @@ import { CloseOutlined, MenuOutlined, UserOutlined } from '@ant-design/icons';
 import Emoji from './Emoji';
 
 import './Resume.css';
+import ResumeEntry from './ResumeEntry';
 
 const { Option } = Select;
 
@@ -27,9 +28,9 @@ function Resume() {
         <div className="container-primary floating-box resume">
             
                 {/* <div style={{ display: 'flex', justifyContent: 'space-between' }}> */}
-                <Tooltip title="Hide" placement="bottom">
+                {/* <Tooltip title="Hide" placement="bottom">
                     <div className="exit-button" onClick={() => setIsOpen(!isOpen)}></div>
-                </Tooltip>
+                </Tooltip> */}
 
                     <div className="container-secondary resume-hero"></div>
                     <div className="container-secondary resume-picture">
@@ -37,10 +38,49 @@ function Resume() {
                     </div>
                     <div className="resume-name">
                     <h1><strong>Jane Doe</strong></h1>
+                    </div>
                     <div className="resume-profile"> 
                     I am a capable and creative computer science student with a flair for problem solving. I have strong technical, interpersonal and communication skills and am aiming to pursue a career in software engineering & design. 
                     </div>
-            </div>
+                    <ResumeEntry title="Skills" display="inline" 
+                        entries={[
+                            {date: "", text: "HTML", subText: ""},
+                            {date: "", text: "CSS", subText: ""},
+                            {date: "", text: "JavaScript", subText: ""},
+                            {date: "", text: "React.js", subText: ""},
+                            {date: "", text: "Python", subText: ""},
+                            {date: "", text: "C", subText: ""},
+                            {date: "", text: "Java", subText: ""},
+                            {date: "", text: "Graphic Design", subText: ""},
+                            {date: "", text: "Photoshop", subText: ""},
+                            {date: "", text: "Illustrator", subText: ""}
+                        ]}
+                    />
+                    <ResumeEntry title="Experience" display="block"
+                        entries={[
+                            {date: "2020", text: "Internship at a Company", subText: "as a Full Stack Developer"},
+                            {date: "2019", text: "Internship at a different Company", subText: "as a Front end Developer"},
+                            {date: "2018-Current", text: "Part time job", subText: ""}
+                        ]}
+                    />
+
+                    <ResumeEntry title="Achievements" display="inline"
+                        entries={[
+                            {date: "2020", text: "First Cass Honours", subText: ""},
+                            {date: "2020", text: "First in Hackathon", subText: ""}
+                        ]}
+                    />
+
+                    <ResumeEntry title="Education" display="block"
+                        entries={[
+                            {date: "2018-20", text: "The University of Melbourne", subText: "Bachelor of Science - Major in computing and software systems"},
+                            {date: "2017", text: "School College", subText: "Completed International Baccalaureate Diploma"}
+                        ]}
+                    />
+
+                    
+                    
+            
                     
                     
 
---
+++ client/src/components/ResumeEntry.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+import { Select, Space, Input, Tooltip } from 'antd';
+// import { CloseOutlined, MenuOutlined, UserOutlined } from '@ant-design/icons';
+
+// import Emoji from './Emoji';
+
+import './ResumeEntry.css';
+
+type  ResumeEntryProps = {
+    title: string,
+    display: string,
+    entries: {date: string, text: string, subText: string}[]
+}
+
+function ResumeEntry(props: ResumeEntryProps) {
+
+    return (
+        <div className={`resume-entry ${props.display == "inline" ? "resume-entry-inline" : "resume-entry-block"}`} >
+            <h2><strong>{props.title}</strong></h2>
+            <ul>
+            {props.entries.map( (value, index, array) => {
+                    return (
+                    // return  <Col flex="0px"> {/* 240px (portfolio object) + 30px (padding) */} 
+                    <li><div className="resume-entry-date">{value.date}</div><div>{value.text}<div className="resume-sub-text">{value.subText}</div></div></li>
+                    )
+                })}
+            </ul>
+        </div>
+            
+    )
+}
+
+export default ResumeEntry;
