4c3d49a74dbd6eb6d56ed08c2ed9243fbf57108f
Nathan Harvey <neathtthefirst@gmail.com>
2020-10-01 23:45:19 +1000

    ✨ Point to correct key and certificate

--- scripts/serveReact.sh
+++ scripts/serveReact.sh
@@ -1,2 +1,2 @@
 cd /home/ec2-user/application/client
-serve -s build -l 443 --ssl-cert /home/ec2-user/certs/server.cert --ssl-key /home/ec2-user/certs/server.key
+serve -s build -l 443 --ssl-cert /etc/letsencrypt/live/glowbal.us.to/fullchain.pem --ssl-key /etc/letsencrypt/live/glowbal.us.to/privkey.pem
