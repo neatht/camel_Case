48992ca1730fa714ecf38fb0513400431c66aa95
Nathan Harvey <neathtthefirst@gmail.com>
2020-10-01 12:08:17 +1000

    🐛 Run start server as root and get correct key for react

--- scripts/startServer.sh
+++ scripts/startServer.sh
@@ -1,8 +1,8 @@
 echo startServer
 pushd /home/ec2-user/application/api
-pm2 start ./dist/index.js --name "APIBackend" --log "/home/ec2-user/application/apilog.txt" --time
+sudo pm2 start ./dist/index.js --name "APIBackend" --log "/home/ec2-user/application/apilog.txt" --time
 popd
 pushd /home/ec2-user/application/scripts
-pm2 start ./serveReact.sh --name "ReactFrontend" --log "/home/ec2-user/application/reactlog.txt" --time
+sudo pm2 start ./serveReact.sh --name "ReactFrontend" --log "/home/ec2-user/application/reactlog.txt" --time
 popd
 echo done
