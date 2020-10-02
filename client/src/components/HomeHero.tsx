f340148eb0f507be3334800692ad232abbd78913
Edward Hinrichsen <edward@pop-os.localdomain>
2020-10-02 18:18:00 +1000

    :lipstick: Center HomeHero text

--- client/src/components/HomeHero.tsx
+++ client/src/components/HomeHero.tsx
@@ -16,7 +16,7 @@ function HomeHero() {
     return(
         <>
             { isOpen
-                ? <div className="home-hero" style={{ backgroundColor: '#fafafa' }}>
+                ? <div className="home-hero">
 
                     {/* <div className="toggle-home-hero" onClick={() => setIsOpen(!isOpen)}> */}
                     <Tooltip title="Hide" placement="bottom">
@@ -25,11 +25,13 @@ function HomeHero() {
                     {/* </div> */}
 
                     <div className="home-CTA" style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", minHeight: 250}}>
-                        <h1><Emoji symbol="📝" /> <strong>ePortfolio</strong></h1> 
-                        <p> Showcase your best work by creating a profile </p>
+                        <h1><Emoji symbol="📝" /> <strong>Glowbal</strong></h1> 
+    
+                        <p> Showcase your best work by creating a profile and...</p>
+                        
                         { 
                             !isAuthenticated
-                            ? <Button type="primary" onClick={() => loginWithRedirect()}> <span style={{paddingRight: 5}}><Emoji symbol="🚀" /></span> Sign Up</Button>
+                            ? <Button type="primary" onClick={() => loginWithRedirect()}> <span style={{paddingRight: 5}}><Emoji symbol="🚀" /></span> <strong>Get Discoverd</strong></Button>
                             : null
                         }
                         </div>
