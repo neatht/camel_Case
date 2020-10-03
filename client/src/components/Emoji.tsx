f2b029048095beb33a3a62f864d8ea9352519d79
Taylor Johnston <taylorjohnston@mail.com>
2020-10-03 15:04:58 +1000

    :wheelchair: Fix Emoji accessibility properties

--- client/src/components/Emoji.tsx
+++ client/src/components/Emoji.tsx
@@ -10,8 +10,8 @@ function Emoji(props: EmojiProps) {
         <span
             className="emoji"
             role="img"
-            aria-label={props.label ? props.label : ""}
-            aria-hidden={props.label ? "false" : "true"}
+            aria-label={props.label ? props.label : undefined}
+            aria-hidden={props.label ? undefined : true}
         >
             {props.symbol}
         </span>
