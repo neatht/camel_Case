0299d3aebad65379412763165974e56dcee48b0f
Edward Hinrichsen <edward@pop-os.localdomain>
2020-10-05 19:22:11 +1100

    :sparkles: Initialize react-beautiful-dnd

--- client/src/components/PortfolioHero.tsx
+++ client/src/components/PortfolioHero.tsx
@@ -10,6 +10,7 @@ import Paragraph from 'antd/lib/typography/Paragraph';
 import { useAuth0 } from '@auth0/auth0-react';
 import { EditOutlined } from '@ant-design/icons';
 import placeholderFolioImage from '../placeholder-folio-image.png';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 
 type PortfolioHeroProps = {
@@ -26,12 +27,26 @@ function PortfolioHero(props: PortfolioHeroProps) {
     const [media, setMedia] = useState(props.media);
     const addMedia = (type: string, url: string, index: number) => {
         if (type === 'image') {
-            return (<div className={`portfolio-hero-media container-secondary ${slide===index ? "portfolio-hero-media-max" : ""}`} style={{backgroundImage: `url(${url})`}}></div>)
-        } else if (type === 'video') {
-            return (<div className={`portfolio-hero-media container-secondary ${slide===index ? "portfolio-hero-media-max" : ""}`} ><video width="100%" height="100%" controls><source src={`${url}`} /></video></div>)
-        } else {
-            return (<div className={`portfolio-hero-media container-secondary ${slide===index ? "portfolio-hero-media-max" : ""}`} ><embed src= {`${url}`} width="100%" height="100%" /></div>)
+            return (
+                <Draggable key={url} draggableId={url} index={index}>
+                    {dragProvided => (
+                        <div
+                            {...dragProvided.dragHandleProps}
+                            {...dragProvided.draggableProps}
+                            ref={dragProvided.innerRef}
+                           
+                        >
+                            <div className={`portfolio-hero-media container-secondary ${slide===index ? "portfolio-hero-media-max" : ""}`} style={{backgroundImage: `url(${url})`}}></div>
+                        </div>
+                    )}
+                </Draggable>
+            );
         }
+        // } else if (type === 'video') {
+        //     return (<div className={`portfolio-hero-media container-secondary ${slide===index ? "portfolio-hero-media-max" : ""}`} ><video width="100%" height="100%" controls><source src={`${url}`} /></video></div>)
+        // } else {
+        //     return (<div className={`portfolio-hero-media container-secondary ${slide===index ? "portfolio-hero-media-max" : ""}`} ><embed src= {`${url}`} width="100%" height="100%" /></div>)
+        // }
 
     }
 
@@ -49,10 +64,30 @@ function PortfolioHero(props: PortfolioHeroProps) {
                 <div className="portfolio-hero-button display-right container-secondary" onClick={
                     () => slide < media.length - 1 ? setSlide(slide+1) : setSlide(0)
                 }>&#8250;</div>
+                <DragDropContext
+                    onDragEnd={({ destination, source }) => {}}
+                >
+                    <div style={{height: "100%"}}>
+                    <Droppable
+                        droppableId={"0"}
+                        type={"CARD"}
+                        direction="horizontal"
+                        isCombineEnabled={false}
+                    >
+                        {dropProvided => (
+                            <div style={{height: "100%"}} {...dropProvided.droppableProps}>
+                                <div style={{height: "100%", display: "flex" }} ref={dropProvided.innerRef}>
+                                    {media.map( (value, index, array) => {
+                                        return addMedia(value.type, value.url, index);
+                                    })}
+                                    {dropProvided.placeholder}
+                                </div>
+                            </div>
+                        )}
+                    </Droppable>
+                    </div>
+                </DragDropContext>
                 
-                {media.map( (value, index, array) => {
-                    return addMedia(value.type, value.url, index);
-                })}
             </div>
         );
     } else {
