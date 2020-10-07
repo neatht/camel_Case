1fa4328ab5cb7f201b71b37ea38299caa9472146
Taylor Johnston <taylorjohnston@mail.com>
2020-10-07 14:02:04 +1100

    🚨 Fix linter issues from merge

--- client/src/components/CompanyAutoComplete.tsx
+++ client/src/components/CompanyAutoComplete.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 
 import { AutoComplete } from 'antd';
 
-
 type CompanyAutoCompleteProps = {
   /** Callback function for when a company is selected */
   onSelect?: (value: string) => void;
@@ -13,41 +12,45 @@ type CompanyAutoCompleteProps = {
 };
 
 function CompanyAutoComplete(props: CompanyAutoCompleteProps) {
-    
-    const [options, setOptions] = useState<Array<any>>();
-    const [value, setValue] = useState(props.initialValue ? props.initialValue : "");
-    
-    async function fetchCompanyAutoComplete(query: string): Promise<Array<any>> {
-        const res = await fetch ('https://autocomplete.clearbit.com/v1/companies/suggest?query=' + query);
-        return res.json();
-    }
-  
-    async function searchResult(query: string) {
+  const [options, setOptions] = useState<Array<any>>();
+  const [value, setValue] = useState(
+    props.initialValue ? props.initialValue : ''
+  );
 
-        const data = await fetchCompanyAutoComplete(query);
-    
-        return data
-            .map((item: any, idx: any) => {
-                return ({
-                    value: (`${item.name}`),
-                    label: (
-                        <div style={{ padding: 5 }}>
-                            <span style={{ paddingRight: 10 }}>
-                                <img alt=" " src={item.logo} width="20px" style={{borderRadius: 10}}/>
-                            </span>
-                            <span>
-                                {item.name}
-                            </span>
-                        </div>),
-                });
-        });
-    };
+  async function fetchCompanyAutoComplete(query: string): Promise<Array<any>> {
+    const res = await fetch(
+      'https://autocomplete.clearbit.com/v1/companies/suggest?query=' + query
+    );
+    return res.json();
+  }
 
-    const handleValueChange = (value: string) => {
-        setValue(value);
-        if (props.onChange) {
-            props.onChange(value);
-        }
+  async function searchResult(query: string) {
+    const data = await fetchCompanyAutoComplete(query);
+
+    return data.map((item: any, idx: any) => {
+      return {
+        value: `${item.name}`,
+        label: (
+          <div style={{ padding: 5 }}>
+            <span style={{ paddingRight: 10 }}>
+              <img
+                alt=" "
+                src={item.logo}
+                width="20px"
+                style={{ borderRadius: 10 }}
+              />
+            </span>
+            <span>{item.name}</span>
+          </div>
+        ),
+      };
+    });
+  }
+
+  const handleValueChange = (value: string) => {
+    setValue(value);
+    if (props.onChange) {
+      props.onChange(value);
     }
   };
 
