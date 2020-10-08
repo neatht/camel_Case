004a1f3fc73493c0ea196e2e3e84f88960b4abc9
Sean Gayler <scianciogayl@student.unimelb.edu.au>
2020-10-08 16:29:24 +1000

    :recycle: move db pool connect to a middleware.

---
+++ api/src/middleware/db.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import { pool } from '../config/postgres';
+
+/**
+ * connectPool() connects to the pg pool.
+ * 
+ * @param req - the express Request object
+ * @param res - the express Response object
+ * @param next - the express NextFunction object
+ */
+export const connectPool = (req: any, res: express.Response,
+                            next: express.NextFunction) => {
+  pool.connect((connectErr: any, client: any, done: any) => {
+    /**
+     * Return an status 500 error message if server is unable to connect with
+     * the database.
+     */
+    if (connectErr) {
+      throw new Error('Error connecting to database')
+    } else {
+      req.poolClient = client;
+      next();
+    }
+}
