b1dae9f91c07ed74464f6cb490e099af38df2a93
Edward Hinrichsen <edward@pop-os.localdomain>
2020-10-12 10:41:10 +1100

    :bug: Fix linter warnings

--- client/src/components/PortfolioHero.tsx
+++ client/src/components/PortfolioHero.tsx
@@ -93,7 +93,7 @@ function PortfolioHero(props: PortfolioHeroProps) {
                       style={{ height: '100%', display: 'flex' }}
                       ref={dropProvided.innerRef}
                     >
-                      {media.map((value, index, array) => {
+                      {media.map((value, index) => {
                         if (value.type === 'image') {
                           return (
                             <Draggable
@@ -206,7 +206,7 @@ function PortfolioHero(props: PortfolioHeroProps) {
           </DragDropContext>
         ) : (
           <div style={{ height: '100%', display: 'flex' }}>
-            {media.map((value, index, array) => {
+            {media.map((value, index) => {
               if (value.type === 'image') {
                 return (
                   <div
