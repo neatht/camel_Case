7da6ab8421fa9030c9d777085d6ce4a4ed6b0f18
Sean Gayler <scianciogayl@student.unimelb.edu.au>
2020-10-12 12:46:25 +1000

    :recycle: move console logging higher in file

--- api/src/middleware/error.ts
+++ api/src/middleware/error.ts
@@ -6,10 +6,10 @@ import express from 'express';
 export const errorHandler = (err: Error, req: express.Request,
                              res: express.Response,
                              next: express.NextFunction) => {
+  console.error(err.message);
   res.status(500);
   res.json({
     status: 'error',
     message: 'Something went wrong on our end. :('
   });
-  console.log(err.message);
 }
