83738b001052f0e69bee736bc620ed142b595327
Sean Gayler <scianciogayl@student.unimelb.edu.au>
2020-10-13 16:40:22 +1000

    :fire: Remove /api/profile/getID route and its controller/service

--- api/src/components/profile/routes.ts
+++ api/src/components/profile/routes.ts
@@ -1,12 +1,11 @@
 import * as express from "express";
-import { getUser, addUser, updateUser, deleteUser, getUserID } from './controller';
+import { getUser, addUser, updateUser, deleteUser } from './controller';
 import { profileValidator } from './validator';
 import { jwtCheck } from '../../middleware/auth';
 import { connectPool } from '../../middleware/db';
 
 const router = express.Router();
 
-router.get('/getID', jwtCheck, connectPool, getUserID);
 router.get('/:id', connectPool, getUser);
 router.post('/', jwtCheck, profileValidator, connectPool, addUser);
 router.put('/', jwtCheck, profileValidator, connectPool, updateUser);
