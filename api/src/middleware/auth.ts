6633f0eac65029e2f96c1783bddf4855aa009c22
Kevin Kim <kwkim@student.unimelb.edu.au>
2020-10-14 13:26:54 +0800

    :sparkles: add owner validator middleware

--- api/src/middleware/auth.ts
+++ api/src/middleware/auth.ts
@@ -6,6 +6,7 @@
 import dotenv from 'dotenv';
 import jwt from 'express-jwt';
 import jwks from 'jwks-rsa';
+import express from 'express';
 
 dotenv.config();
 
@@ -31,3 +32,24 @@ audience: process.env.AUTH0_AUDIENCE,
 issuer: process.env.AUTH0_ISSUER,
 algorithms: ['RS256']
 });
+
+
+/**
+ * checkIsOwner() checks whether the user ID attached to req.user.sub after
+ * calling jwtCheck() is the same as the user ID sent in the request body
+ * (req.body.userID).
+ *
+ * @param req - the express Request object
+ * @param res - the express Response object
+ * @param next - the express NextFunction object
+ */
+export const checkIsOwner = (req: any, res: express.Response, next: express.NextFunction) => {
+  // sub attribute in the format of "<social platform>|<user id>"
+  const ID = req.user.sub.split('|')[1];
+
+  if (parseInt(ID, 0) === parseInt(req.body.userID, 0)) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+}
