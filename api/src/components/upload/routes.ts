e599266b935d9fe2a13c86d89c07c3580a1ef395
Kevin Kim <kwkim@student.unimelb.edu.au>
2020-10-14 13:25:10 +0800

    :sparkles: add media upload and get

--- api/src/components/upload/routes.ts
+++
@@ -1,9 +0,0 @@
-import * as express from 'express';
-import { signS3 } from './controller';
-import { jwtCheck } from '../../middleware/auth';
-
-const router = express.Router();
-
-router.post('/', jwtCheck, signS3);
-
-export default router;
