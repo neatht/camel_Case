cfffbf11a21b58576c5413dfa35e32e8f30c6dd4
Sean Gayler <scianciogayl@student.unimelb.edu.au>
2020-10-14 16:19:49 +1000

    :fire: Remove checkIsOwner() and implications

--- api/src/components/experience/routes.ts
+++ api/src/components/experience/routes.ts
@@ -2,12 +2,12 @@ import * as express from "express";
 import { addExperience, updateExperience, getExperiences, deleteExperience, getOwnExperiences } from './controller';
 import { experienceValidator } from './validator';
 import { checkValidation } from '../../middleware/validator';
-import { jwtCheck, checkIsOwner } from '../../middleware/auth';
+import { jwtCheck } from '../../middleware/auth';
 import { connectPool } from '../../middleware/db';
 
 const router = express.Router();
 
-router.get('/GetOwnExperiences', jwtCheck, checkIsOwner, connectPool, getOwnExperiences);
+router.get('/GetOwnExperiences', jwtCheck, connectPool, getOwnExperiences);
 router.get('/:userID', connectPool, getExperiences);
 router.post('/', jwtCheck, experienceValidator, checkValidation, connectPool, addExperience); // Add validation
 router.put('/', jwtCheck, experienceValidator, checkValidation, connectPool, updateExperience); // Add validation
--- api/src/components/profile/routes.ts
+++ api/src/components/profile/routes.ts
@@ -2,12 +2,12 @@ import * as express from "express";
 import { getUser, addUser, updateUser, deleteUser, getOwnUser } from './controller';
 import { profileValidator } from './validator';
 import { checkValidation } from '../../middleware/validator';
-import { jwtCheck, checkIsOwner } from '../../middleware/auth';
+import { jwtCheck } from '../../middleware/auth';
 import { connectPool } from '../../middleware/db';
 
 const router = express.Router();
 
-router.get('/getOwnProfile', jwtCheck, checkIsOwner, connectPool, getOwnUser);
+router.get('/getOwnProfile', jwtCheck, connectPool, getOwnUser);
 router.get('/:userID', connectPool, getUser);
 router.post('/', jwtCheck, profileValidator, checkValidation, connectPool, addUser);
 router.put('/', jwtCheck, profileValidator, checkValidation, connectPool, updateUser);
