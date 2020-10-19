6c6da55d2885bcb1210228b857cae724c7dc111e
Sean Gayler <scianciogayl@student.unimelb.edu.au>
2020-10-20 09:42:53 +1000

    :sparkles: Add logic to deal with public_location to getUser controller.

--- api/src/components/profile/controller.ts
+++ api/src/components/profile/controller.ts
@@ -30,22 +30,39 @@ export const getUser = async (req: any, res: express.Response, next: express.Nex
       });
     } else {
       if (result.public) {
-        res.status(200);
-        return res.json({
-          status: 'success',
-          data: {
-            firstName: result.first_name,
-            lastName: result.last_name,
-            bio: result.bio,
-            location: result.location,
-            lookingForWork: result.looking_for_work,
-            public: result.public,
-            gender: result.gender,
-            DOB: result.date_of_birth
-          }
-        });
+        if (result.public_location) {
+          res.status(200);
+          return res.json({
+            status: 'success',
+            data: {
+              firstName: result.first_name,
+              lastName: result.last_name,
+              bio: result.bio,
+              location: result.location,
+              lookingForWork: result.looking_for_work,
+              public: result.public,
+              gender: result.gender,
+              DOB: result.date_of_birth,
+            }
+          });
+        } else {
+          res.status(200);
+          return res.json({
+            status: 'success',
+            data: {
+              firstName: result.first_name,
+              lastName: result.last_name,
+              bio: result.bio,
+              lookingForWork: result.looking_for_work,
+              public: result.public,
+              gender: result.gender,
+              DOB: result.date_of_birth,
+            }
+          });
+        }
+
       } else {
-        res.status(401);
+        res.status(200);
         return res.json({
           status: 'success',
           data: {
