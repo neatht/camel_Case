7f4e0452913f3794f13cc7362dbee5108f138c90
Sean Gayler <scianciogayl@student.unimelb.edu.au>
2020-10-20 15:36:25 +1000

    :bug: Force educationID on getOwnEducationService and getEducationService to be a string.

--- api/src/components/education/service.ts
+++ api/src/components/education/service.ts
@@ -22,6 +22,7 @@ export const getOwnEducationService = async (req: any, res: express.Response, ne
   education_id as "educationID" FROM education WHERE user_id=$1;';
   const queryParams: any[] = [req.user.sub.split('|')[1]];
   const queryResult: any = await service(req, next, query, queryParams);
+  queryResult.educationID = queryResult.educationID.toString();
   return queryResult.rows;
 }
 
@@ -41,6 +42,7 @@ export const getEducationService = async (req: any, res: express.Response, next:
   education_id as "educationID" FROM education WHERE user_id=$1;';
   const queryParams: any[] = [req.params.userID];
   const queryResult: any = await service(req, next, query, queryParams);
+  queryResult.educationID = queryResult.educationID.toString();
   return queryResult.rows;
 }
 
